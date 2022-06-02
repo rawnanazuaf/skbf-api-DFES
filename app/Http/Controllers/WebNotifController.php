1097c94bac36cbe3c1d9edc2ad289a928c7129b8
Muhammad Fauzan Anwar <82807039+rawnanazuaf@users.noreply.github.com>
2022-06-02 11:30:40 +0700

    Update WebNotifController.php
    API

--- app/Http/Controllers/WebNotifController.php
+++ app/Http/Controllers/WebNotifController.php
@@ -22,8 +22,7 @@ class WebNotifController extends Controller
     {
         $firebaseToken = User::whereNotNull('device_token')->pluck('device_token')->all();
           
-        $SERVER_API_KEY = 'AAAAV37eLV4:APA91bG1tFPkxTdTj8vlOJZq_aiGdX3_ydZrolt3_gMxyFlSAua3tka5Vm4wBC0QePJlVryT7Kjh9HgqsQVIpVlvoy9a5-L6p288JW5M-VIAjhDhGcKsriNj2YhwF2CSH0k5AIJ90_6r';
-  
+        $SERVER_API_KEY = config('api.FCM_API_KEY');
         $data = [
             "registration_ids" => $firebaseToken,
             "notification" => [
